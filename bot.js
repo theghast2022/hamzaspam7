983d84886ff18428e407b2f7604a5da5fcaa8401
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-10 21:28:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
 
 
 client.on('ready', async() => {
-var server = "510887418874953737"; // ايدي السررفر
-var channel = "510887418874953739";//ايدي الروم
+var server = "510888031520161806"; // ايدي السررفر
+var channel = "510888032149176320";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast, The_Ghast, The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , ')
     },305);
@@ -31,4 +31,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
   }
  });
 
-client.login('NTEwODk3NjkwNTQ0OTYzNTk4.DsjCLA.SzD6t8kl_cH8BDplJBExc65JsCY');
+client.login('NTEwOTEyMTk5MDUzNTQxMzc2.DsjQng.QQPp9tm_Xlb8sVnf5hLRzsAUn4I');
